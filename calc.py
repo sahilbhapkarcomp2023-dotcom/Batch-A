30c55340479b7e57c4e746fb49a3e1828ab6a6f7
Sahil Bhapkar <sahil.bhapkar.comp.2023@vpkbiet.org>
2026-02-05 10:24:30 +0000

    add substraction

--- calc.py
+++ calc.py
@@ -2,5 +2,7 @@ a =int(input("Enter the number A: "))
 b = int(input("Enter the number B: "))
 sum = a + b
 print("Sum is:", sum)
+sub = a - b
+print("Substraction is:", sub)
 
 print("THis is the calc code.")
